ee6a2cf7e1a2cb48bf79619d468308f7af12dc95
Matias Vasquez <m.vasquezac@gmail.com>
2025-05-06 12:37:13 -0400

    feat: declare error api on ts definition

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,9 +1,17 @@
 interface ElectronAPI {
-    onUpdateClientCount: (callback: (count: number) => void) => void;
-    onUpdateClientLog: (callback: (data: string) => void) => void;
-    onUpdatePosStatus: (callback: (posConnected: boolean) => void) => void;
+  onUpdateClientCount: (callback: (count: number) => void) => void;
+  onUpdateClientLog: (callback: (data: string) => void) => void;
+  onUpdatePosStatus: (callback: (posConnected: boolean) => void) => void;
+}
+
+interface ErrorApi {
+  onError: (
+    callback: ({ errorTitle: string, errorMessage: string }) => void
+  ) => void;
+  closeApp: () => void;
 }
 
 interface Window {
-    electronAPI: ElectronAPI;
+  electronAPI: ElectronAPI;
+  errorAPI: ErrorApi;
 }
